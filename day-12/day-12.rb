7cef7b0e30c8da1e437da5bf77418d4e8c998244
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2018-12-21 11:21:59 -0800

    Add files via upload

---
+++ day-12/day-12.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+################################################################################
+# Copyright (C) 2018 Michael Wiseman                                           #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify it      #
+# under the terms of the GNU Affero General Public License as published by the #
+# Free Software Foundation, either version 3 of the License, or (at your       #
+# option) any later version.                                                   #
+#                                                                              #
+# This program is distributed in the hope that it will be useful, but WITHOUT  #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License #
+# for more details.                                                            #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
+################################################################################
+require 'json'
+
+# Function definitions
+
+def part_one(input)
+  f = File.read(input)
+  data = JSON.parse(f)
+  puts data.values
+end
+
+def part_two(ipt)
+
+end
+
+input_file = 'day-12-input.json'
+
+# Hand did this
+part_one(input_file) # 119433
+# part_two(input_file) # 68466
